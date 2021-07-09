2ebf8f59bffa41b6c792dd5ebef20a649fba030b
ilimkanu <ilimkanu@gmail.com>
2021-07-09 12:50:21 +0600

    42.1

---
+++ src/Counter.java
@@ -0,0 +1,16 @@
+public class Counter {
+    public Counter() {
+    }
+
+    public int countChar(String str, char c)
+    {
+        int count = 0;
+
+        for(int i=0; i < str.length(); i++)
+        {    if(str.charAt(i) == c)
+            count++;
+        }
+
+        return count;
+    }
+}
